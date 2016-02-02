d764aa55e03ef6d3512b9c60c87c81a7efb21682
Le Thanh Son <thsonvt@gmail.com>
2016-02-03 06:25:47 +0800

    Step 1 done

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,7 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell : UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        cell.textLabel?.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
